049ef1f5f1455961eaacd4bd88bcdb3b9a812b17
Pablo Quisbert <pabloquisberty2k@gmail.com>
2025-03-10 11:48:27 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, flash
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.secret_key = os.getenv('FLASK_SECRET_KEY', '123')
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    try:
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        mensaje = request.form['mensaje']
+        
+        msg = Message('Nuevo mensaje de contacto',
+                      sender=app.config['MAIL_USERNAME'],
+                      recipients=[app.config['MAIL_USERNAME']])
+        msg.body = f"Nombre: {nombre}\nCorreo: {correo}\nMensaje: {mensaje}"
+        
+        mail.send(msg)
+        flash("mensaje enviado correctamente", "success")
+    except ImportError as e:
+        print(f"Error: {e}")
+        flash("Ocurrio un error al enviar el mensaje, intentelo mas tarde", "danger")
+        
+    return render_template('index.html')
+
+if __name__== "__main__":
+    app.run(debug=True)
